9b3c4b9cfd417394fbe36126e7815862c1b1b9f3
quarbby <lynnette-ng@outlook.com>
2018-05-19 14:07:57 +0800

    Add starter

---
+++ Vue_Full_Project/src/containers/Fullhome.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <AppHeaderHome/>
+    <div class="app-body">
+      <Sidebar :navItems="nav"/>
+      <main class="main">
+        <breadcrumb :list="list"/>
+        <div class="container-fluid">
+          <router-view></router-view>
+        </div>
+      </main>
+      <AppAside/>
+    </div>
+    <AppFooter/>
+  </div>
+</template>
+
+<script>
+import nav from '../_nav'
+import { HeaderHome as AppHeaderHome, Sidebar, Aside as AppAside, Footer as AppFooter, Breadcrumb } from '../components/'
+
+export default {
+  name: 'fullhome',
+  components: {
+    AppHeaderHome,
+    Sidebar,
+    AppAside,
+    AppFooter,
+    Breadcrumb
+  },
+  data () {
+    return {
+      nav: nav.items
+    }
+  },
+  computed: {
+    name () {
+      return this.$route.name
+    },
+    list () {
+      return this.$route.matched
+    }
+  }
+}
+</script>
